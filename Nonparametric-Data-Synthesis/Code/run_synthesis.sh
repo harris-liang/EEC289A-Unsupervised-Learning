1ba6abb0ba6f03f5434e49a2855a5311b017fb47
Chenye Yang <cyyyang@ucdavis.edu>
2024-05-02 20:52:11 -0700

    mnist synthesis

--- Nonparametric-Data-Synthesis/Code/run_synthesis.sh
+++ Nonparametric-Data-Synthesis/Code/run_synthesis.sh
@@ -1,1 +1,1 @@
-python Nonparametric-Data-Synthesis/Code/synthesis.py --sample_path=Nonparametric-Data-Synthesis/Code/Textures/mnist.png --out_path=Nonparametric-Data-Synthesis/Result/mnist.png --window_height=1120 --window_width=1120 --kernel_size=11 --visualize
+python Nonparametric-Data-Synthesis/Code/synthesis.py --sample_path=Nonparametric-Data-Synthesis/Code/Textures/mnist.png --out_path=Nonparametric-Data-Synthesis/Result/mnist.png --window_height=1000 --window_width=1000 --kernel_size=11
